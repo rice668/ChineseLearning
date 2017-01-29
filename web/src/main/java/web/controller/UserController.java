c97425a6cf3341c47ee0d08e8ca015bb1a1914a6
zhangminglei <zml13856086071@163.com>
2017-01-29 20:28:50 +0800

    A stable status for next to resolve JSON data and set into a User

--- web/src/main/java/web/controller/UserController.java
+++ web/src/main/java/web/controller/UserController.java
@@ -6,6 +6,7 @@ import com.west.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -19,9 +20,8 @@ public class UserController {
   UserService userService;
 
   @RequestMapping(value = "/addUser", method = RequestMethod.POST)
-  public String addUser(@ModelAttribute("user") User user) {
-    userService.addUser(user);
-    return "countryDetails";
+  public Result addUser(@RequestBody String data) {
+    return new Result(true);
   }
 
 }
